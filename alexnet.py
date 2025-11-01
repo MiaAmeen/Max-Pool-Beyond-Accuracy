fixed bug in pruning mask calculation, dtype mismatch

alexnet.py
@@ -60,10 +60,10 @@ class AlexNet(nn.Module):
             old_mask = layer.weight_mask
         else:
             weights = layer.weight.data
-            old_mask = torch.ones_like(weights, dtype=torch.bool)
+            old_mask = torch.ones_like(weights)
 
         threshold = quality_param * torch.std(weights).item()
-        mask = torch.abs(weights) > threshold
+        mask = (torch.abs(weights) > threshold).int()
         prune.custom_from_mask(layer, name="weight", mask=mask & old_mask)
         
         if remove: prune.remove(layer, 'weight')
@@ -75,12 +75,8 @@ class AlexNet(nn.Module):
         return torch.sum(w == 0) / w.numel()
 
 
-# if __name__ == "__main__":
-#     model = AlexNet()
-    # print(model.check_sparsity(model.conv_layers[0]))
-    # model.prune(model.conv_layers[0], quality_param=0.5)
-    # print(model.check_sparsity(model.conv_layers[0]))
-    # model.prune(model.conv_layers[0], quality_param=0.5)
-    # print(model.check_sparsity(model.conv_layers[0]))
-    # model.prune(model.conv_layers[0], quality_param=0.5)
-    # print(model.check_sparsity(model.conv_layers[0]))
+if __name__ == "__main__":
+    model = AlexNet()
+    model.prune(model.conv_layers[0], quality_param=0.5)
+    print(model.check_sparsity(model.conv_layers[0]))
+    model.prune(model.conv_layers[0], quality_param=0.5)

main.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 from alexnet import AlexNet
 import os
 
-
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 # -------------------------
@@ -152,9 +151,7 @@ def iterative_prune_train_retrain(model, model_path, dataset, trainloader, testl
 
         retrain_acc = evaluate(model, testloader)
 
-        print(f"{prune_iter+1}, {', '.join(f'{sparsity:.4f}' for sparsity in conv_sparsities)}, \
-              {', '.join(f'{sparsity:.4f}' for sparsity in fc_sparsities)}, \
-                {base_acc}, {pruned_acc}, {retrain_acc}")
+        print(f"{prune_iter+1}, {', '.join(f'{sparsity:.4f}' for sparsity in conv_sparsities)}, {', '.join(f'{sparsity:.4f}' for sparsity in fc_sparsities)}, {base_acc}, {pruned_acc}, {retrain_acc}")
         
         base_acc = retrain_acc
 